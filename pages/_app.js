7a6f5979880437185e5533f09747743888bf019d
akshata <makshata403@gmail.com>
2023-03-01 11:35:10 +0100

    particle network connected

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,58 @@
-
-import '@rainbow-me/rainbowkit/styles.css';
+import "../styles/globals.css";
 import {
-  getDefaultWallets,
-  RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+    ModalProvider,
+    evmWallets,
+    Optimism,
+    Polygon,
+    Ethereum,
+    EthereumGoerli,
+} from "@particle-network/connect-react-ui";
+import { WalletEntryPosition } from "@particle-network/auth";
+import "@particle-network/connect-react-ui/dist/index.css";
 
+function MyApp({ Component, pageProps }) {
+    if (typeof window !== "undefined") {
+        //TODO: particle internal test, remove it before publish
+        window.__PARTICLE_DEVELOPMENT__ = true;
+    }
 
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
-const { chains, provider } = configureChains(
-  [mainnet, polygon, optimism, arbitrum],
-  [
-    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
-    publicProvider()
-  ]
-);
-const { connectors } = getDefaultWallets({
-  appName: 'My RainbowKit App',
-  chains
-});
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider
-})
-
-
-
-
-export default function App({ Component, pageProps }) {
-  return (<WagmiConfig client={wagmiClient}>
-  <RainbowKitProvider chains={chains}>
-    <Component {...pageProps}/>
-  </RainbowKitProvider>
-</WagmiConfig>)
+    return (
+        <ModalProvider
+            walletSort={["Particle Auth", "Wallet"]}
+            particleAuthSort={[
+                "email",
+                "phone",
+                "google",
+                "apple",
+                "facebook",
+                "microsoft",
+                "linkedin",
+                "github",
+                "discord",
+            ]}
+            options={{
+                projectId: "fa0fe0e8-f1bb-47ff-88bb-4fa31711e7b3",
+                clientKey: "cKtHdeHis0ghNom64w7mdNkGYX5rmRr0jLlIKatY",
+                appId: "859fada8-48ad-441b-b8e6-cde15ae1b48f",
+                chains: [
+                  Optimism,
+                  Polygon,
+                  Ethereum,
+                  EthereumGoerli,
+                ],
+                particleWalletEntry: {
+                    displayWalletEntry: true,
+                    defaultWalletEntryPosition: WalletEntryPosition.BR,
+                    supportChains: [Ethereum, EthereumGoerli],
+                },
+                wallets: [...evmWallets({ qrcode: false })],
+            }}
+            language="en"
+            theme={"light"}
+        >
+            <Component {...pageProps} />
+        </ModalProvider>
+    );
 }
+
+export default MyApp;
